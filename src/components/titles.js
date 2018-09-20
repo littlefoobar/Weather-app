13b6c7ca37987db3018e3dd5c5f0ccf917f55e38
littlefoobar <littlefoobar@gmail.com>
2018-09-20 15:28:28 +0800

    commiting all files

---
+++ src/components/titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+            <div>
+               <h1 className="title-container__title">Track Climate</h1>
+               <h3 className="title-container__subtitle">Findout temperature, conditions and more...</h3>
+            </div>
+        );
+
+export default Titles;
